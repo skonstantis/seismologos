291ac5bbdb866c23ee8489a67badc18a25602887
skonstantis <skonstantis@uth.gr>
2024-09-04 11:41:32 +0300

    root html page addition

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const reportsRoutes = require("./reports");
+const path = require("path");
 
 const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../pages/index.html"));
+  });
 
 router.use("/reports", reportsRoutes);
 
