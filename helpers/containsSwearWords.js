9fddc0b6823530283f20c875f93c6771320f249d
skonstantis <skonstantis@uth.gr>
2024-09-27 15:53:22 +0300

    fix

--- helpers/containsSwearWords.js
+++ helpers/containsSwearWords.js
@@ -1,8 +1,13 @@
 const { checkEnglishText, checkGreekText } = require("bad-words-checker");
 
-export const containsSwearWord = (text) => {
+const containsSwearWord = (text) => {
   const isEnglishProfane = checkEnglishText(text).isProfane;
   const isGreekProfane = checkGreekText(text).isProfane;
 
   return isEnglishProfane || isGreekProfane;
 };
+
+
+module.exports = {
+    containsSwearWord,
+  };  
