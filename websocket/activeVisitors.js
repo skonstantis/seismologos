ea9c1327af182ad51aa5fd1dc1a481db61cc7ef5
skonstantis <skonstantis@uth.gr>
2024-10-08 20:14:18 +0300

    fix

--- websocket/activeVisitors.js
+++ websocket/activeVisitors.js
@@ -8,13 +8,22 @@ module.exports = async (ws, req, db, logger, visitorId) => {
 
         await db.collection("stats").updateOne({}, { $set: { activeVisitors: activeVisitors.size } });
 
+        const currentStats = await db.collection("stats").findOne({});
+
+        broadcastMessage(currentStats.json());
+
         ws.on("message", (message) => {
         });
 
         ws.on("close", async () => {
             activeVisitors.delete(visitorId);
             logger.info(`Visitor ${visitorId} disconnected from the WebSocket`);
+
             await db.collection("stats").updateOne({}, { $set: { activeVisitors: activeVisitors.size } });
+
+            const currentStats = await db.collection("stats").findOne({});
+
+            broadcastMessage(currentStats.json());
         });
 
         ws.on("error", (error) => {
@@ -25,5 +34,11 @@ module.exports = async (ws, req, db, logger, visitorId) => {
     } catch (error) {
         logger.error("WebSocket error:", error);
         ws.close(4000, "Internal server error");
+    }
+};
+
+const broadcastMessage = (message) => {
+    for (const [ws] of activeVisitors) {
+            ws.send(message);
     }
 };
