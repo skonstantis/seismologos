56f1432eacf2c5f5a42695eaf8d98ca13e460ddd
skonstantis <skonstantis@uth.gr>
2024-10-23 11:59:01 +0300

    fix

---
+++ websocketSetup.js
@@ -0,0 +1,10 @@
+const WebSocket = require("ws");
+const websocketRouter = require('./websocket');
+
+module.exports = (httpServer, db, logger, activeUsers, activeVisitors) => {
+  const wsServer = new WebSocket.Server({ server: httpServer });
+
+  wsServer.on("connection", (ws, req) => {
+    websocketRouter(ws, req, db, logger, activeUsers, activeVisitors);
+  });
+};
